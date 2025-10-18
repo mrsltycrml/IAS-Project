567d813a156dc6c82d34c25cc1bff025d45e3521
mrsltycrml <carlosbusuego15@gmail.com>
2025-10-18 12:59:50 +0800

    Add comprehensive favicon configuration with explicit links and favicon.ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon.png" type="image/png" />
+        <link rel="shortcut icon" href="/icon.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/icon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
